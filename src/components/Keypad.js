38ebe01dcb0d09fa95f86d686c256f7e3fe54c4c
JawaraGordon <jawaragordon@icloud.com>
2022-05-05 12:33:20 -0400

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React, { useState } from 'react';
+
+function Keypad() {
+  const [password, setPassword] = useState('');
+  const enterPassword = () => {
+    console.log('Entering password...');
+  };
+  return (
+    <div>
+      <input
+        onChange={enterPassword}
+        type="password"
+        name="search"
+        // onChange={handleChange}
+        placeholder="Enter Password"
+      />
+    </div>
+  );
+}
+
+export default Keypad;
